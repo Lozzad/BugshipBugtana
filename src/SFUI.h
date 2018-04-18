811e3c6091f17770881df4b430cb1f25fb2a4da1
ld469 <ld469@MOCO205L-010.university.brighton.ac.uk>
2018-04-18 13:01:39 +0100

    Still god awful

---
+++ src/SFUI.h
@@ -0,0 +1,18 @@
+#ifndef SFTEXTURE_H
+#define SFTEXTURE_H
+
+#include <string>
+#include <stdio.h>
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+using namespace std;
+
+class SFUI {
+public:
+private:   
+};
+
+#endif
